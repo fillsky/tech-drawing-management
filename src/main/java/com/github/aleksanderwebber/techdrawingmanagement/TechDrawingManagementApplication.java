d593328d507b6ead4c6285c40ea99ddf6fc2c40a
Filip Skiba <filipskiba@gmail.com>
2019-11-21 18:13:49 +0100

    Inital commit: Spring Initialization

---
+++ src/main/java/com/github/aleksanderwebber/techdrawingmanagement/TechDrawingManagementApplication.java
@@ -0,0 +1,13 @@
+package com.github.aleksanderwebber.techdrawingmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechDrawingManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechDrawingManagementApplication.class, args);
+	}
+
+}
