60101aa216bf1b4c7fd2d8383aae99474bbc0b77
Aleksander Weber <aleksander.weber@icloud.com>
2019-11-21 18:21:16 +0100

    name correction

--- src/main/java/com/github/aleksanderweber/techdrawingmanagement/TechDrawingManagementApplication.java
+++ src/main/java/com/github/aleksanderweber/techdrawingmanagement/TechDrawingManagementApplication.java
@@ -1,4 +1,4 @@
-package com.github.aleksanderwebber.techdrawingmanagement;
+package com.github.aleksanderweber.techdrawingmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/github/aleksanderweber/techdrawingmanagement/TechDrawingManagementApplicationTests.java
+++ src/test/java/com/github/aleksanderweber/techdrawingmanagement/TechDrawingManagementApplicationTests.java
@@ -1,4 +1,4 @@
-package com.github.aleksanderwebber.techdrawingmanagement;
+package com.github.aleksanderweber.techdrawingmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
